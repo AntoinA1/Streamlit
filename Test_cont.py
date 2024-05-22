80717b504b3c1da4d9de9dd7a2f66f782106b634
AntoinA1 <142228731+AntoinA1@users.noreply.github.com>
2024-05-22 08:33:42 +0000

    Fonctionne mais problème 1 similarité

--- Test_cont.py
+++ Test_cont.py
@@ -1,33 +1,38 @@
 import pandas as pd
 import streamlit as st
 from loaders import load_items, load_ratings
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Chargement des données
 movies_df = load_items()
-ratings_df = load_ratings(surprise_format=False)  # Sans surprise format pour manipulation
+ratings_df = load_ratings(surprise_format=False)
 
-print(movies_df.head())
 
 # One-hot encoding des genres
 genres_df = movies_df['genres'].str.get_dummies(sep='|')
 
-# Fusionner les notes avec les genres
-ratings_with_genres = ratings_df.merge(genres_df, left_on='movieId', right_index=True)
+print(genres_df.head())
 
-# Initialiser la matrice de features pour les utilisateurs
-user_ids = ratings_df['userId'].unique()
-df_features = pd.DataFrame(0, index=user_ids, columns=genres_df.columns)
+# Calcul de la similarité cosinus entre les films
+film_similarity = cosine_similarity(genres_df)
 
-# Remplir la matrice avec les notes moyennes des genres par utilisateur
-for user_id in user_ids:
-    user_ratings = ratings_with_genres[ratings_with_genres['userId'] == user_id]
-    for genre in genres_df.columns:
-        genre_ratings = user_ratings[user_ratings[genre] == 1]['rating']
-        if not genre_ratings.empty:
-            df_features.at[user_id, genre] = genre_ratings.mean()
+# Demander à l'utilisateur d'entrer l'ID d'un film qu'il a aimé
+selected_movie_id = st.text_input("Entrez l'ID du film que vous avez aimé :")
 
-# Afficher la matrice one-hot encoding
-st.title("Matrice One-Hot Encoding des Genres")
-st.write(df_features)
+# Vérifier si l'ID du film est valide
+if selected_movie_id is not None and selected_movie_id.isdigit() and int(selected_movie_id) in movies_df.index:
+    selected_movie_id = int(selected_movie_id)
+
+    # Trouver les films similaires à celui que l'utilisateur a aimé
+    similar_movies = pd.Series(film_similarity[selected_movie_id], index=movies_df.index).sort_values(ascending=False)[1:]
+
+    # Afficher les films recommandés à l'utilisateur
+    st.header("Films recommandés :")
+    for i, (movie_id, similarity_score) in enumerate(similar_movies.head(5).items(), start=1):
+        movie_title = movies_df.loc[movie_id, 'title']
+        st.write(f"{i}. {movie_title} (Similarité : {similarity_score:.2f})")
+else:
+    st.error("Veuillez entrer un ID de film valide.")
+
+
 
