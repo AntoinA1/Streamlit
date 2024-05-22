767e1a8b553eb9283103cfbe76eae11673623955
AntoinA1 <142228731+AntoinA1@users.noreply.github.com>
2024-05-22 09:04:26 +0000

    Fonctionne combinaison des deux

--- app_5.py
+++ app_5.py
@@ -12,17 +12,9 @@ from surprise import AlgoBase, PredictionImpossible
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.ensemble import RandomForestRegressor
 import random as rd
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-# Chargement des données
-movies_df = load_items()
-ratings_data = load_ratings(surprise_format=True)
-trainset = ratings_data.build_full_trainset()
-
-# Créer un dictionnaire qui mappe les IDs des films aux titres correspondants
-movie_id_to_title = dict(zip(movies_df.index, movies_df['title']))
-
-# Classe Recommender pour User-Based
 class UserBasedRecommender:
     def __init__(self, sim_options={}, k=3, min_k=4):
         self.sim_options = sim_options
@@ -49,161 +41,136 @@ class UserBasedRecommender:
 
     def get_user_recommendations(self, user_id, n=10):
         return self.recommend_items(user_id, n)
-    
-class ContentBased():
-    def __init__(self, trainset, features_method, regressor_method):
-        self.trainset = trainset
-        self.regressor_method = regressor_method
-        self.content_features = self.create_content_features(features_method)
-        self.user_profiles = {}
 
-    def create_content_features(self, features_method):
-        df_items = load_items()
-        if features_method is None:
-            df_features = None
-        elif features_method == "title_length":
-            df_features = df_items['title'].apply(lambda x: len(x)).to_frame('n_character_title')
-        elif features_method == "release_year":
-            df_items['release_year'] = df_items['title'].str.extract(r'\((\d{4})\)')
-            df_items['release_year'] = df_items['release_year'].astype(float)
-            df_features = df_items[['release_year']]
-        elif features_method == "genres":
-            df_features = df_items['genres'].str.get_dummies(sep='|')
+
+class ContentBased:
+    def __init__(self):
+        # Chargement des données
+        self.movies_df = load_items()
+        self.ratings_df = load_ratings(surprise_format=False)
+
+        # One-hot encoding des genres
+        self.genres_df = self.movies_df['genres'].str.get_dummies(sep='|')
+
+        # Calcul de la similarité cosinus entre les films
+        self.film_similarity = cosine_similarity(self.genres_df)
+
+    def recommend_movies(self, selected_movie_id):
+        # Vérifier si l'ID du film est valide
+        if selected_movie_id is not None and selected_movie_id.isdigit() and int(
+                selected_movie_id) in self.movies_df.index:
+            selected_movie_id = int(selected_movie_id)
+            selected_movie_title = self.movies_df.loc[selected_movie_id, 'title']
+
+            # Calculer le nombre de genres du film sélectionné
+            num_genres_selected_movie = self.genres_df.loc[selected_movie_id].sum()
+
+            # Trouver les films similaires à celui que l'utilisateur a aimé
+            similar_movies = pd.Series(self.film_similarity[selected_movie_id], index=self.movies_df.index).sort_values(
+                ascending=False)[1:]
+
+            # Afficher le titre du film demandé
+            st.write(f"Voici les films les plus similaires à '{selected_movie_title}' :")
+
+            # Afficher les films recommandés à l'utilisateur
+            num_recommendations = 0
+            for i, (movie_id, similarity_score) in enumerate(similar_movies.items(), start=1):
+                movie_title = self.movies_df.loc[movie_id, 'title']
+
+                # Calculer le nombre de genres du film actuellement examiné
+                num_genres_movie = self.genres_df.loc[movie_id].sum()
+
+                # Normaliser la similarité par la racine du produit des nombres de genres des deux films
+                weighted_similarity_score = similarity_score / (
+                            (num_genres_selected_movie * num_genres_movie) ** 0.5)
+
+                st.write(f"{i}. {movie_title} (Similarité pondérée : {weighted_similarity_score:.2f})")
+                num_recommendations += 1
+                if num_recommendations >= 20:
+                    break
         else:
-            raise NotImplementedError(f'Feature method {features_method} not yet implemented')
-        return df_features
+            st.error("Veuillez entrer un ID de film valide.")
 
-    def fit(self, trainset):
-        
-        self.user_profile = {u: None for u in trainset.all_users()}
-
-        for u in self.user_profile:
-            user_data = []
-
-            for inner_iid, rating in trainset.ur[u]:
-                raw_iid = trainset.to_raw_iid(inner_iid)
-                user_data.append({'item_id': raw_iid, 'user_ratings': rating})
-
-            df_user = pd.DataFrame(user_data)
-            df_user = df_user.merge(
-                self.content_features,
-                how='left',
-                left_on='item_id',
-                right_index=True
-            )
-
-            genre_columns = [col for col in df_user.columns if col != 'item_id' and col != 'user_ratings']
-            X = df_user[genre_columns].values
-            y = df_user['user_ratings'].values
-
-            if self.regressor_method == 'linear_regression':
-                regressor = LinearRegression()
-            elif self.regressor_method == 'ridge_regression':
-                regressor = Ridge(alpha=1)
-            elif self.regressor_method == 'lasso_regression':
-                regressor = Lasso(alpha=1.0)
-            elif self.regressor_method == 'random_forest':
-                regressor = RandomForestRegressor(n_estimators=100, random_state=42)
-            else:
-                raise NotImplementedError(f'Regressor method {self.regressor_method} not yet implemented')
-
-            regressor.fit(X, y)
-            self.user_profile[u] = regressor
-
-    def estimate(self, u, i):
-        if not (self.trainset.knows_user(u) and self.trainset.knows_item(i)):
-            raise PredictionImpossible('User and/or item is unknown.')
-
-        user_regressor = self.user_profile[u]
-        raw_item_id = self.trainset.to_raw_iid(i)
-        item_features = self.content_features.loc[raw_item_id:raw_item_id, :].values
-
-        score = user_regressor.predict(item_features.reshape(1, -1))[0]
-        score = max(0.5, min(score, 5))
-        return score
-
-    def get_user_recommendations(self, user_id, n=10):
-        anti_test_set = self.trainset.build_anti_testset()
-        predictions = self.test(anti_test_set)
-        user_recommendations = defaultdict(list)
-        for uid, iid, _, est, _ in predictions:
-            user_recommendations[uid].append((iid, est))
-        if user_id in user_recommendations:
-            user_recommendations[user_id].sort(key=lambda x: x[1], reverse=True)
-            return user_recommendations[user_id][:n]
-        else:
-            return []
-
-test = ContentBased.create_content_features("ContentBased", 'genres')
 
 # Utiliser le dictionnaire pour obtenir le titre du film par son ID
 def get_movie_title_by_id(movie_id, movie_id_to_title):
     return movie_id_to_title.get(movie_id, "Titre non trouvé")
 
-# Initialiser l'état de la session pour stocker les nouvelles notations
-if 'new_ratings' not in st.session_state:
-    st.session_state['new_ratings'] = []
+
+# Chargement des données
+movies_df = load_items()
+ratings_data = load_ratings(surprise_format=True)
+trainset = ratings_data.build_full_trainset()
+
+# Créer un dictionnaire qui mappe les IDs des films aux titres correspondants
+movie_id_to_title = dict(zip(movies_df.index, movies_df['title']))
 
 # Titre de l'application
 st.title("Recommender System, Group 6")
-
-# Section pour l'insertion des films et des notes pour un nouvel utilisateur
-st.header("Nouveau utilisateur - Entrez vos films et notes")
-
-movie_query = st.text_input("Entrez le nom d'un film que vous avez vu")
-if movie_query:
-    movie_choices = movies_df['title'].tolist()
-    search_results = process.extract(movie_query, movie_choices, limit=5)
-    selected_movie = st.selectbox("Sélectionnez un film :", [result[0] for result in search_results])
-    if selected_movie:
-        rating = st.slider(f"Notez le film {selected_movie}", 0.5, 5.0, 3.0, step=0.5)
-        if st.button("Ajouter ce film et sa note"):
-            movie_id = movies_df[movies_df['title'] == selected_movie].index[0]
-            st.session_state['new_ratings'].append((movie_id, rating))
-            st.success(f"Ajouté: {selected_movie} - {rating}")
-
-# Afficher les films et les notes ajoutés
-if st.session_state['new_ratings']:
-    st.header("Films notés")
-    for i, (movie_id, rating) in enumerate(st.session_state['new_ratings']):
-        st.write(f"Film : {get_movie_title_by_id(movie_id, movie_id_to_title)}, Note : {rating}")
-        if st.button(f"Supprimer la notation {i+1}", key=f"del_{i}"):
-            del st.session_state['new_ratings'][i]
-            st.success("Notation supprimée avec succès !")
-            st.experimental_rerun()
 
 # Choisir le type de système de recommandation
 st.header("Choisissez le type de système de recommandation")
 rec_type = st.selectbox("Type de recommandation", ["User-Based", "Content-Based"])
 
-# Création de l'instance du Recommender en fonction du choix
 if rec_type == "User-Based":
-    recommender = UserBasedRecommender(sim_options={'name': 'cosine', 'user_based': True, 'min_support': 3}, k=3, min_k=4)
-elif rec_type == "Content-Based":
-    recommender = ContentBased(trainset=trainset, features_method= "genres", regressor_method= "ridge_regression" )
+    # Section pour l'insertion des films et des notes pour un nouvel utilisateur
+    st.header("Nouveau utilisateur - Entrez vos films et notes")
 
-# Entraîner le modèle avec les données initiales
-recommender.fit(ratings_data)
+    movie_query = st.text_input("Entrez le nom d'un film que vous avez vu")
+    if movie_query:
+        movie_choices = movies_df['title'].tolist()
+        search_results = process.extract(movie_query, movie_choices, limit=5)
+        selected_movie = st.selectbox("Sélectionnez un film :", [result[0] for result in search_results])
+        if selected_movie:
+            rating = st.slider(f"Notez le film {selected_movie}", 0.5, 5.0, 3.0, step=0.5)
+            if st.button("Ajouter ce film et sa note"):
+                movie_id = movies_df[movies_df['title'] == selected_movie].index[0]
+                st.session_state['new_ratings'].append((movie_id, rating))
+                st.success(f"Ajouté: {selected_movie} - {rating}")
 
-# Bouton pour générer les recommandations
-if st.button("Obtenir des recommandations"):
+    # Afficher les films et les notes ajoutés
     if st.session_state['new_ratings']:
-        new_ratings_df = pd.DataFrame(st.session_state['new_ratings'], columns=['movieId', 'rating'])
-        new_ratings_df['userId'] = ratings_data.df['userId'].max() + 1
+        st.header("Films notés")
+        for i, (movie_id, rating) in enumerate(st.session_state['new_ratings']):
+            st.write(f"Film : {get_movie_title_by_id(movie_id, movie_id_to_title)}, Note : {rating}")
+            if st.button(f"Supprimer la notation {i+1}", key=f"del_{i}"):
+                del st.session_state['new_ratings'][i]
+                st.success("Notation supprimée avec succès !")
+                st.experimental_rerun()
 
-        combined_ratings = pd.concat([ratings_data.df, new_ratings_df], ignore_index=True)
-        reader = Reader(rating_scale=(0.5, 5.0))
-        combined_data = Dataset.load_from_df(combined_ratings[['userId', 'movieId', 'rating']], reader)
+    # Entraîner le modèle avec les données initiales
+    recommender = UserBasedRecommender(sim_options={'name': 'cosine', 'user_based': True, 'min_support': 3}, k=3,
+                                       min_k=4)
+    recommender.fit(ratings_data)
 
-        recommender.fit(combined_data)
+    # Bouton pour générer les recommandations
+    if st.button("Obtenir des recommandations"):
+        if st.session_state['new_ratings']:
+            new_ratings_df = pd.DataFrame(st.session_state['new_ratings'], columns=['movieId', 'rating'])
+            new_ratings_df['userId'] = ratings_data.df['userId'].max() + 1
 
-        user_id = new_ratings_df['userId'].iloc[0]
-        recommendations = recommender.get_user_recommendations(user_id)
+            combined_ratings = pd.concat([ratings_data.df, new_ratings_df], ignore_index=True)
+            reader = Reader(rating_scale=(0.5, 5.0))
+            combined_data = Dataset.load_from_df(combined_ratings[['userId', 'movieId', 'rating']], reader)
 
-        if recommendations:
-            st.header("Recommandations personnalisées")
-            for movie_id, rating in recommendations:
-                movie_title = get_movie_title_by_id(movie_id, movie_id_to_title)
-                st.write(f"Film recommandé : {movie_title}, Estimation : {rating}")
-        else:
-            st.warning("Aucune recommandation trouvée pour cet utilisateur.")
+            recommender.fit(combined_data)
+
+            user_id = new_ratings_df['userId'].iloc[0]
+            recommendations = recommender.get_user_recommendations(user_id)
+
+            if recommendations:
+                st.header("Recommandations personnalisées")
+                for movie_id, rating in recommendations:
+                    movie_title = get_movie_title_by_id(movie_id, movie_id_to_title)
+                    st.write(f"Film recommandé : {movie_title}, Estimation : {rating}")
+            else:
+                st.warning("Aucune recommandation trouvée pour cet utilisateur.")
+
+elif rec_type == "Content-Based":
+    # Section pour l'insertion de l'ID du film pour un nouvel utilisateur
+    st.header("Nouveau utilisateur - Entrez l'ID du film que vous avez aimé :")
+
+    selected_movie_id = st.text_input("Entrez l'ID du film que vous avez aimé :")
+    if selected_movie_id:
+        content_recommender = ContentBased()
+        content_recommender.recommend_movies(selected_movie_id)
